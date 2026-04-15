f20f34909f931f5a10c0498126b445720fd48bbb
OdariKCharles1 <daymondodari68@gmail.com>
2026-04-15 16:19:07 +0300

    feat(robot_control): add ros2 node initialization to robot_control script
    Initialize ROS2 node and log a test message before shutting down.

--- src/robot_control/robot_control/robot_control.py
+++ src/robot_control/robot_control/robot_control.py
@@ -1,2 +1,10 @@
 #!usr/bin/env python3
 
+import rclpy
+from rclpy.node import Node
+
+def main():
+    rclpy.init()
+    node=Node("python_node")
+    node.get_logger().info("Testing robot_control.... in ROS2")
+    rclpy.shutdown()

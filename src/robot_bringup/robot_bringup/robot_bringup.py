74b337f7338c05d00e2e2acf729b15742868e01a
OdariKCharles1 <daymondodari68@gmail.com>
2026-04-23 06:05:50 +0300

    refactor(bringup): implement RobotBringupNode class
    Refactor the main entry point to use a dedicated Node class instead of
    instantiating a generic Node. This improves structure and allows for
    proper lifecycle management using rclpy.spin().

--- src/robot_bringup/robot_bringup/robot_bringup.py
+++ src/robot_bringup/robot_bringup/robot_bringup.py
@@ -2,9 +2,18 @@
 import rclpy
 from rclpy.node import Node
 
+class RobotBringupNode(Node):
+    def __init__(self):
+        super().__init__("Robot_bringup_node")
+        self.get_logger().info("Robot Bringup node running...")
+    
+    
 def main(args=None):
     rclpy.init(args=args)
-    node=Node("python_node")
-    node.get_logger().info("Testing node.... in ROS2")
+    node=RobotBringupNode()
+    rclpy.spin(node)
     rclpy.shutdown()
 
+
+if __name__=="__main__":
+    main()

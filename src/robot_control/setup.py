a54e46c1947c22b0ff820471ed22ed3138888deb
OdariKCharles1 <daymondodari68@gmail.com>
2026-04-23 05:33:06 +0300

    chore(robot_control): remove unnecessary whitespace in setup.py

--- src/robot_control/setup.py
+++ src/robot_control/setup.py
@@ -6,6 +6,11 @@ setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
+    
+    
+    
+    
+    
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
